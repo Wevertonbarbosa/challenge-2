b2fee50b581ad297c939b46b790e49cbe8b9d09a
Wevertonbarbosa <wevertonlbs00@gmail.com>
2024-09-11 20:50:25 -0300

    Testando a branch

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -2,6 +2,6 @@ package br.com.fiap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Testando a branch");
     }
 }
